b0f092dc3814ea952210bbf8c532c1dc09f24666
David North <david.north@oc.edu>
2023-10-04 21:34:59 -0500

    feat (eventPieceSignup) : add field firstPiece

--- app/models/eventType.model.js
+++ app/models/eventType.model.js
@@ -30,6 +30,10 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.BOOLEAN,
       allowNull: false,
     },
+    firstPiece: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+    },
     status: {
       type: Sequelize.STRING,
       allowNull: false,
