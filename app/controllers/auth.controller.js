98d92efffaecd18de509dc3d91b234f71db2e302
JuliaFlores21 <julia.flores@eagles.oc.edu>
2023-10-28 20:32:38 -0500

    more changes to include dbs

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -65,10 +65,12 @@ exports.login = async (req, res) => {
 
         await UserRole.findAll({
           where: { userId: user.id, status: "Active" },
-          include: {
+          include: [{
             model: Role,
           },
-        })
+          {model: User},
+          {model: Major}
+        ]})
           .then((data) => {
             user.roles = data;
           })
